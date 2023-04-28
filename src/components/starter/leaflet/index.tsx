9f445d46e8a3e03be1dd5cef176e97d366197bd8
Anartz Mugika Ledo <mugan86@gmail.com>
2023-04-28 18:21:37 +0200

    feat: leaflet implement in qwik 0.104.0

---
+++ src/components/starter/leaflet/index.tsx
@@ -0,0 +1,89 @@
+import {
+  component$,
+  useVisibleTask$,
+  useStyles$,
+  useSignal,
+  noSerialize,
+} from "@builder.io/qwik";
+import * as L from "leaflet";
+
+
+import { defaultIcon, drinkWaterIcon, otherIcon } from "~/utils/icons/default";
+import { mapZoomConfigs, getBoundaryBox, addMapControls } from "~/helpers";
+
+import leafletStyles from "./../../../../node_modules/leaflet/dist/leaflet.css?inline";
+import leafletMarkerCluster from "./marker-cluster.css?inline";
+
+export const LeafletMap = component$(({ location }: any) => {
+  useStyles$(leafletStyles);
+  useStyles$(leafletMarkerCluster);
+  const mapContainer$ = useSignal<L.Map>();
+
+  useVisibleTask$(async ({ track }) => {
+    track(location);
+    const pointCenter = location.data.location;
+    
+    if (location && window) {
+      if (mapContainer$.value) {
+        mapContainer$.value.remove();
+      }
+      await import("leaflet.markercluster");
+
+      const markersToCluster = new L.MarkerClusterGroup();
+
+      const centerPosition: L.LatLngExpression = [pointCenter[0], pointCenter[1]];
+      const map = new L.Map("map").setView(
+        centerPosition,
+        location.data.zoom || 13
+      );
+
+      // Take bounds
+
+      const bounds = getBoundaryBox(map);
+      location.data.boundaryBox = bounds;
+
+      mapZoomConfigs(map, location.data);
+
+      const centralPoint = L.marker(centerPosition, {
+        icon: defaultIcon,
+      })
+        .addTo(map)
+        .bindPopup(`<h1>${location.data.name}</h1>`);
+
+      map &&
+        location.pois.length &&
+        location.pois.map((element: any) => {
+          const markerItem = L.marker([element.lat, element.lon], {
+            icon:
+              element.tags.amenity === "drinking_water"
+                ? drinkWaterIcon
+                : otherIcon,
+          });
+          markerItem.bindPopup(
+            `
+            <h2>${element.tags.name || "Sin definir"}</h2>
+            <p>Tag: ${element.tags.amenity || "Sin definir tipo"}</p>
+          `
+          );
+          markerItem.addTo(markersToCluster);
+        });
+
+        map.addLayer(markersToCluster);
+
+      location.pois.length &&
+        map.fitBounds([
+          [centralPoint.getLatLng().lat, centralPoint.getLatLng().lng],
+          ...location.pois.map(
+            (point: any) => [point.lat, point.lon] as [number, number]
+          ),
+        ]);
+
+      // Controls
+
+      addMapControls(map);
+
+      mapContainer$.value = noSerialize(map);
+    }
+  });
+  return <div id="map"></div>;
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,15 +5,34 @@ import {
   useStore,
   useStyles$,
   useTask$,
-} from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+} from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
 import { isServer } from "@builder.io/qwik/build";
 
-import { serverOSMPOIs } from '~/api/osm';
-import { poisTypes } from '~/data/poi-list';
+import { serverOSMPOIs } from "~/api/osm";
+import { LeafletMap } from "~/components/starter/leaflet";
+import { poisTypes } from "~/data/poi-list";
 
 export default component$(() => {
   useStyles$(`
+  #map {
+    margin-top: 1rem;
+    height: 450px;
+    border: 5px solid var(--custom-blue);
+  }
+
+  .leaflet-popup-content h1 {
+    color: black;
+  }
+
+  .leaflet-control-layers-list {
+    text-align: left;
+  }
+
+  .select {
+    background-color: grey !important;
+    color: whitesmoke !important;
+  }
     button {
       width: 250px;
       margin: 5px;
@@ -38,35 +57,41 @@ export default component$(() => {
     .no-checked {
       color: grey
     }
+
+    details {
+      margin-top: 1rem;
+    }
+    details:hover {
+      cursor:pointer;
+    }
   
   `);
 
-  const location = useStore({
-    data: {
-      name: 'Soraluze',
-      location: [43.17478, -2.41172],
-      boundaryBox:
-        '43.14658914559456,-2.4765586853027344,43.202923523094725,-2.3467826843261723',
-      zoom: 13,
+  const location = useStore(
+    {
+      data: {
+        name: "Soraluze",
+        location: [43.17478, -2.41172],
+        boundaryBox:
+          "43.14658914559456,-2.4765586853027344,43.202923523094725,-2.3467826843261723",
+        zoom: 13,
+      },
+      pois: [],
+      select: {
+        name: "Soraluze",
+        location: [43.17478, -2.41172],
+        boundaryBox:
+          "43.14658914559456,-2.4765586853027344,43.202923523094725,-2.3467826843261723",
+        zoom: 13,
+      }.name,
     },
-    pois: [],
-    select: {
-      name: 'Soraluze',
-      location: [43.17478, -2.41172],
-      boundaryBox:
-        '43.14658914559456,-2.4765586853027344,43.202923523094725,-2.3467826843261723',
-      zoom: 13,
-    }.name,
-  }, { deep: true });
+    { deep: true }
+  );
   const pois = useStore(poisTypes, { deep: true });
 
   const poiSelectChange = $((index: number) => {
     pois[index].checked = !pois[index].checked;
   });
-
-  const checkedPoiOptions = useComputed$(() =>
-    pois.filter((option) => option.checked)
-  );
 
   const poisSelected = useComputed$(() => {
     return pois
@@ -80,62 +105,62 @@ export default component$(() => {
       location.pois = (
         await serverOSMPOIs(location.data, poisSelected.value)
       ).osmServices;
-      console.log(location.pois)
+      console.log(location.pois);
     }
   });
   return (
     <>
-      <div class='container container-center'>
+      <div class="container container-center">
         <h3>
-          Selecciona los <span class='highlight'>Puntos de Interés (POIs)</span>
+          Selecciona los <span class="highlight">Puntos de Interés (POIs)</span>
           <br /> deseados
         </h3>
         <br />
         {pois.map((option, index) => (
           <button
             key={option.label}
-            class={option.checked ? 'checked' : 'no-checked'}
+            class={option.checked ? "checked" : "no-checked"}
             onClick$={() => poiSelectChange(index)}
           >
             {option.label}
           </button>
         ))}
-        <h4>Selección</h4>
-        {checkedPoiOptions.value.map((option, index) => (
-          <span key={option.label}>
-            {option.label}{' '}
-            {index < checkedPoiOptions.value.length - 1 ? ',' : ''}
-          </span>
-        ))}
-        <h4>Filtros a aplicar</h4>
-        {JSON.stringify(poisSelected.value)}
         <br />
-        <button
-          onClick$={async () => {
-            const boundaryBox: string =
-              '43.14658914559456,-2.50265121459961,43.202923523094725,-2.3206901550292973';
-            console.log('Lo que se va a mandar');
-            console.log({ boundaryBox }, poisSelected.value);
-            location.pois = (await serverOSMPOIs({ boundaryBox }, poisSelected.value))
-                .osmServices
-            ;
-          }}
-        >
-          Obtener info
-        </button>
-        <h4>Resultado</h4>
-        <div><code>{JSON.stringify(location.pois)}</code></div>
+        {poisSelected.value.length === 0 ? (
+          <p>Debes de seleccionar un tipo de POI para hacer la búsqueda </p>
+        ) : (
+          <button
+            onClick$={async () => {
+              const boundaryBox: string =
+                "43.14658914559456,-2.50265121459961,43.202923523094725,-2.3206901550292973";
+              console.log("Lo que se va a mandar");
+              console.log({ boundaryBox }, poisSelected.value);
+              location.pois = (
+                await serverOSMPOIs({ boundaryBox }, poisSelected.value)
+              ).osmServices;
+            }}
+          >
+            Obtener info
+          </button>
+        )}
+        <details>
+          <summary>Pulsa para ver el resultado en formato JSON</summary>
+          <div>
+            <code>{JSON.stringify(location.pois)}</code>
+          </div>
+        </details>
+        <LeafletMap location={location} />        
       </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "Welcome to Qwik",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content: "Qwik site description",
     },
   ],
 };
